89614b80b51cfa20d046ac9f9a895bbb39ee7ae0
bytetang <jie@celer.network>
2021-07-28 18:21:35 +0800

    update site

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210727.2259';
+const cacheName = 'chirpy-20210728.1819';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
